fb7286e9d02609f44885083ca0b4f7236c9f2bdd
Imanol <ivilardev@gmail.com>
2020-06-09 18:45:56 +0200

    Url Change

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,3 +1,7 @@
-const url = 'http://127.0.0.1:8000';
+let url = 'https://ivilardev-walletapp.herokuapp.com/';
+
+if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
+  url = 'http://127.0.0.1:8000';
+}
 
 export default url;
